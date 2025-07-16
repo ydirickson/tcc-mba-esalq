447ff8a6c6ed9f6ec707e9da4284bffb47d041a4
Yuri Dirickson <yuri.dirickson@usp.br>
2025-07-15 21:34:19 -0300

    Commit inicial

---
+++ graduacao/src/main/java/br/usp/exemplo/graduacao/GraduacaoApplication.java
@@ -0,0 +1,13 @@
+package br.usp.exemplo.graduacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduacaoApplication.class, args);
+	}
+
+}
---
+++ graduacao/src/test/java/br/usp/exemplo/graduacao/GraduacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usp.exemplo.graduacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
